40c615634730210b3b6ea28a14aa079b8d7713fc
yodd <creatyoo0491@gmail.com>
2024-07-05 22:53:57 +0900

    router

---
+++ src/routes/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDiCK8Gc8-5UwpDunnyRPVLdrckDkIY4pg",
+  authDomain: "nwitter-go.firebaseapp.com",
+  projectId: "nwitter-go",
+  storageBucket: "nwitter-go.appspot.com",
+  messagingSenderId: "7358801775",
+  appId: "1:7358801775:web:99168eb82554063435aa9a"
+};
+
+const app = initializeApp(firebaseConfig);
